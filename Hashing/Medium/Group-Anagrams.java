295dcc4f0f92712af7c73d84badc56342407954b
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-07-16 10:33:38 +0530

    Group Anagrams - without sorting, using hash value

--- Hashing/Medium/Group-Anagrams.java
+++ Hashing/Medium/Group-Anagrams.java
@@ -1,16 +1,18 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for(String st : strs){
-            char[] arr = st.toCharArray();
-            Arrays.sort(arr);
-            String sorted = new String(arr);
-            
-            if(!map.containsKey(sorted)){
-                map.put(sorted, new ArrayList<>());
+        if(strs == null || strs.length == 0) 
+            return Collections.emptyList();
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s: strs){
+            char[] frequencyArr = new char[26];
+            for(int i = 0;i<s.length();i++){
+                frequencyArr[s.charAt(i) - 'a']++;
             }
-            map.get(sorted).add(st);;
+            String key = new String(frequencyArr);
+            List<String> tempList = map.getOrDefault(key, new LinkedList<String>());
+            tempList.add(s);
+            map.put(key,tempList);
         }
-        return new ArrayList<>(map.values());
+        return new LinkedList<>(map.values());
     }
 }
