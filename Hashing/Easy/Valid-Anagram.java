701733bf3368cdf89fa3e7425411199fcd59da1a
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-07-24 00:39:33 +0530

    Given two strings s and t, return true if t is an anagram of s

--- Hashing/Easy/Valid-Anagram.java
+++ Hashing/Easy/Valid-Anagram.java
@@ -3,13 +3,14 @@ class Solution {
         if(s.length() != t.length())
             return false;
         HashMap<Character, Integer> map = new HashMap<>();
-        for(int i = 0; i<s.length(); i++){
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-            map.put(t.charAt(i), map.getOrDefault(t.charAt(i), 0) - 1);
-        }
+        for(char ch : s.toCharArray())
+            map.put(ch, map.getOrDefault(ch, 0)+1);
         
-        for(Character key : map.keySet()){
-            if(map.get(key)!=0)
+        for(char ch : t.toCharArray()){
+            if(map.containsKey(ch) && map.get(ch)!=0){
+                map.put(ch, map.get(ch)-1);
+            }
+            else
                 return false;
         }
         return true;
