99899309ed20b92ea472f5f5cf242ee8b93d544d
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-08-07 23:53:22 +0530

    return the minimum number of mutations needed to mutate a string from start to end(BFS)

--- Graphs/Medium/Minimum-Genetic-Mutation.java
+++ Graphs/Medium/Minimum-Genetic-Mutation.java
@@ -1,31 +1,32 @@
 class Solution {
     public int minMutation(String start, String end, String[] bank) {
-        return dfs(start, end, bank, new HashSet<>());
-    }
-    
-    int dfs(String start, String end, String[] bank, Set<String> visited) {
-        if (start.equals(end)) 
-            return 0;
-        
-        int min = Integer.MAX_VALUE;
+        Queue<String> queue = new LinkedList<>();
+        char[] chars = {'A', 'C', 'G', 'T'};
+        Set<String> bankSet = new HashSet<>();
+        int level = 0;
+        queue.offer(start);
         for (String b : bank) {
-            int diff = 0;
-            for (int i = 0; i < start.length(); i++) {
-                if (start.charAt(i) != b.charAt(i)) 
-                    diff++;
-                if (diff > 1) 
-                    break;
-            }
-            if (diff == 1 && !visited.contains(b)) {
-                visited.add(b);
-                int h = dfs(b, end, bank, visited);
-                if (h >= 0) {
-                    min = Math.min(min, 1 + h);
-                }
-                visited.remove(b);
-            }
+            bankSet.add(b);
         }
-        
-        return min == Integer.MAX_VALUE ? -1 : min;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-- > 0) {
+                String currString = queue.poll();
+                if (currString.equals(end)) return level;
+                for (int i = 0; i < currString.length(); i++) {
+                    for (char ch: chars) {
+                        char[] currChars = currString.toCharArray();
+                        currChars[i] = ch;
+                        String modString = new String(currChars);
+                        if (bankSet.contains(modString)) {
+                            queue.add(modString);
+                            bankSet.remove(modString);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
+        return -1;
     }
 }
