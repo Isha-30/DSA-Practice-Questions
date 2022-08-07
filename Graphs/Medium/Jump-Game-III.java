67340eafe582a3394d6950354e50a2b280ef9c44
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-08-07 22:21:26 +0530

    Jump Game III - check if you can reach to any index with value 0.(BFS)

---
+++ Graphs/Medium/Jump-Game-III.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        int n = arr.length;
+        HashSet<Integer> visited = new HashSet<>(); // visited set
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        while (!q.isEmpty()) {
+            int i = q.poll();
+            if (arr[i] == 0) 
+                return true; // found then return it
+            if (!visited.contains(i)){
+                visited.add(i);
+                if (i + arr[i] < n)
+                    q.add(i + arr[i]);
+                if (i - arr[i] >= 0)
+                    q.add(i - arr[i]);
+            }
+            
+        }
+        return false;// not found
+    }
+}
