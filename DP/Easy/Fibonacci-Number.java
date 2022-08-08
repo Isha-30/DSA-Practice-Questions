c0c755e829d81762d69eec60710725025f9953de
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-08-08 10:39:27 +0530

    Fibonacci Number - Recursive, Iterative, DP solution

--- DP/Easy/Fibonacci-Number.java
+++ DP/Easy/Fibonacci-Number.java
@@ -38,3 +38,18 @@ class Solution {
             return dp[n] = fib(n-1)+fib(n-2);
     }
 }
+
+class Solution {
+    //DP - Bottom Up Approach
+    
+    public int fib(int n) {
+        if(n<=1)
+            return n;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+}
