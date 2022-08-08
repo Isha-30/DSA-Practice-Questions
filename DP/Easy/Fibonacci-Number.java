87114a1a40bf550c33744b83e2f7228fb1a78218
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-08-08 10:37:14 +0530

    Fibonacci - DP - Top-Down Approach

--- DP/Easy/Fibonacci-Number.java
+++ DP/Easy/Fibonacci-Number.java
@@ -23,3 +23,18 @@ class Solution {
         else return fib(n-1)+fib(n-2);     
     }
 }
+
+
+class Solution {
+    //DP - Top Down Approach
+    
+    int[] dp = new int[31];
+    public int fib(int n) {
+        if(n<=1)
+            return n;
+        if(dp[n] != 0)
+            return dp[n];
+        else
+            return dp[n] = fib(n-1)+fib(n-2);
+    }
+}
