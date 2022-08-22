43931b20ad647dae73583513dd2cee6930f7cb65
Isha-30 <80475223+Isha-30@users.noreply.github.com>
2022-08-22 23:17:32 +0530

    Return true if we can partition an array into 2 subsets which have equal sum

---
+++ DP/Medium/Partition-Equal-Subset-Sum.java
@@ -0,0 +1,32 @@
+public class Solution {
+	public static boolean canPartition(int[] arr, int n) {
+		int totalSum = 0;
+        for(int i : arr)
+            totalSum += i;
+        if(totalSum %2 != 0)
+            return false;
+        
+        int k = totalSum/2;
+        
+        boolean[][] dp = new boolean[n][k+1];
+
+        for(int i = 0; i<n; i++){
+            dp[i][0] = true;
+        }
+
+        if(arr[0] <= k)
+            dp[0][arr[0]] = true;
+
+        for(int ind = 1; ind<n; ind++){
+            for(int target = 1; target <= k; target++){
+                boolean notTake = dp[ind-1][target];
+                boolean take = false;
+                if(arr[ind] <= target)
+                    take = dp[ind-1][target - arr[ind]];
+
+                dp[ind][target] = take || notTake;
+            }   
+        }
+        return dp[n-1][k];
+	}
+}
